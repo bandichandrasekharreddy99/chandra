b09f98d8f2772729474d769187204f5ae7693c1f
bandichandrasekharreddy99 <41770243+bandichandrasekharreddy99@users.noreply.github.com>
2018-07-27 15:37:38 +0530

    Update prob5.py

--- prob5.py
+++ prob5.py
@@ -3,7 +3,7 @@
 n = raw_input()
 if(n >= 'a' and n <= 'z') or (n >= 'A' and n <= 'Z'):
 	if n in ['a','e','i','o','u','A','E','I','O','U']:
-		print('vowel')
+		print('Vowel')
 	else:
 		print('Consonant')
 else:
